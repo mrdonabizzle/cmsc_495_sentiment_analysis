3bfc9fb650ec780347729b77598f197664170beb
eric sabelhaus <eric@sabel.haus>
2017-05-23 21:24:49 -0400

    adding some inital code to get things rolling

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      // define the files to lint
+      files: ['./lib/**/*.js'],
+      // configure JSHint (documented at http://www.jshint.com/docs/)
+      options: {
+        maxdepth: 4,
+        maxcomplexity: 6,
+        strict: true,
+        undef: false,
+        eqeqeq: true,
+        esnext: true
+      },
+    },
+    plato: {
+      coverage: {
+        options: {
+          maxdepth: 4,
+          maxcomplexity: 6,
+          strict: true,
+          undef: false,
+          eqeqeq: true
+        },
+        files: {
+          'plato': ['lib/**/*.js']
+        }
+      }
+    },
+    mochaTest: {
+      test: {
+        src: ['test/**/*.js'],
+        options: {
+          captureFile: 'mocha/results.txt'
+        }
+      }
+    }
+  });
+  //grunt
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
+  //individual testing tasks
+  grunt.registerTask('plato', ['plato:coverage']);
+  grunt.registerTask('mocha', ['mochaTest']);
+  grunt.registerTask('coverage', ['shell']);
+
+  //load packages
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-plato');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+}
