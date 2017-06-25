3dcb0400227189ec7df9ea27eb1c737c3fa276ff
Eric Sabelhaus <eric@sabel.haus>
2017-06-25 14:34:16 -0400

    make sample test work

--- test/queue_test.js
+++ test/queue_test.js
@@ -5,8 +5,25 @@ var chai = require('chai'),
 var Q = require('../lib/sentiment_app/queue');
 var log_factory = require('../lib/sentiment_app/logger');
 
-describe("QUEUE:TEST [what are we testing about it]", function() {
-  it("does the thing we expect", function(done) {
+var jobs = {
+  "add": {
+    plugins: [ 'jobLock', 'retry' ],
+    pluginOptions: {
+      jobLock: {},
+      retry: {
+        retryLimit: 3,
+        retryDelay: (1000 * 5),
+      }
+    },
+    perform: function(a,b,callback){
+      var answer = a + b;
+      callback(null, answer);
+    }
+  }
+}
+
+describe("QUEUE:TEST with a queue", function() {
+  it("enqueues a simple job", function(done) {
     Q.queue({
       "pkg": "ioredis",
       "host": "127.0.0.1",
@@ -14,9 +31,14 @@ describe("QUEUE:TEST [what are we testing about it]", function() {
       "port": 6379,
       "database": 0
     }, // this is the connection configuration for a queue
-    {}, // this is the jobs available
+    jobs, // this is the jobs available
     log_factory('debug'), // this is the logger
     function(_queue) { // this is the callback which contains the actual queue
+      _queue.connect(function(){
+        // finally, we enqueue the requested analysis job
+        _queue.enqueue('sentiment', 'add', [1,2]);
+        done();
+      });
       // within this space we test that the queue works
     });
   });
